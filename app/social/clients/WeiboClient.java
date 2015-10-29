48cdb9ba8334f884b40e90a4242723fd81e7e403
Sasinda <sp2335@cornell.edu>
2015-10-29 16:39:30 -0400

    wireframe for getting Weibo Oauth token and profile

---
+++ app/social/clients/WeiboClient.java
@@ -0,0 +1,38 @@
+package social.clients;
+
+import org.pac4j.core.client.BaseClient;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.oauth.client.BaseOAuth20Client;
+import org.pac4j.oauth.credentials.OAuthCredentials;
+import org.scribe.model.Token;
+import social.clients.profile.WeiboProfile;
+
+/**
+ * Created by sasinda on 10/29/15.
+ */
+public class WeiboClient extends BaseOAuth20Client<WeiboProfile> {
+    @Override
+    protected boolean requiresStateParameter() {
+        return false;
+    }
+
+    @Override
+    protected boolean hasBeenCancelled(WebContext context) {
+        return false;
+    }
+
+    @Override
+    protected String getProfileUrl(Token accessToken) {
+        return null;
+    }
+
+    @Override
+    protected WeiboProfile extractUserProfile(String body) {
+        return null;
+    }
+
+    @Override
+    protected BaseClient<OAuthCredentials, WeiboProfile> newClient() {
+        return null;
+    }
+}
---
+++ app/social/clients/profile/WeiboProfile.java
@@ -0,0 +1,10 @@
+package social.clients.profile;
+
+import model.CommonProfile;
+import org.pac4j.oauth.profile.OAuth20Profile;
+
+/**
+ * Created by sasinda on 10/29/15.
+ */
+public class WeiboProfile extends OAuth20Profile {
+}
