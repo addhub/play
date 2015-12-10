2ca9b03ec26845e2065b11c161d41fee20abaf77
Sasinda <sp2335@cornell.edu>
2015-12-09 22:49:09 -0500

    changed pic folder

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,7 +18,7 @@ public class Application extends Controller {
     UserService userService = new UserService();
     S3Service s3Service = new S3Service();
 
-    public static final String PICTURE_FOLDER ="/Users/Shared/upload/";
+    public static final String PICTURE_FOLDER ="~/Shared/upload/";
 
 
     public Result viewIndex() {
